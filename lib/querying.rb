d12af233b58da9387e2aada26f7d8859464a9d4e
Naya  Willis <greedybrain18@gmail.com>
2019-11-06 22:11:24 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -40,5 +40,14 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT c.name, COUNT(cb.book_id) 
+  FROM characters c 
+  JOIN character_books cb 
+    ON c.id = cb.character_id
+  GROUP BY(cb.book_id);"
 end
+
+
+
+
+
